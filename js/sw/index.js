9cc308b2638872b195a8525f1177b7bc1a6152de
chrisnc00 <chrisnc00@gmail.com>
2018-06-10 18:16:04 -0400

    Initial responsive and service worker changes

---
+++ js/sw/index.js
@@ -0,0 +1,45 @@
+var staticCacheName = 'reviews-stage-one-1';
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(staticCacheName).then(function(cache) {
+            return cache.addAll([
+                '',
+                '',
+                ''
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith('reviews-stage-one-') &&
+                           cacheName != staticCacheName;
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event){
+    var requestUrl = new URL(event.request.url);
+
+    if (requestUrl.origin === location.origin) {
+        if (requestUrl.pathname === '/') {
+            event.respondWith(caches.match('/skeleton'));
+            return;
+        }
+    }
+
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
